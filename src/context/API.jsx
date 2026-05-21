4bf46cafc0081d5e676b8d2c9d55eba2d1c20e01
Ancedi <patrickhombrebueno@gmail.com>
2026-05-21 13:50:17 +0200

    Implemented barebone start for search function

--- src/context/API.jsx
+++ src/context/API.jsx
@@ -1,4 +1,4 @@
-async function GetCats(){
+export default async function GetCats(){
     const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=30&has_breeds=1&api_key=live_h8YWhscJZVWoGk9IshWtNApDx9WMXumpAH8aQcBHXoRxFNnsoL5c60KQRigQvrJV');
     if (!response.ok){
         throw new Error(`HTTP Error: ${response.status}`);
--- src/pages/Catalogue.jsx
+++ src/pages/Catalogue.jsx
@@ -1,3 +1,27 @@
-export default function Catalogue(){
-    return <h1>Catalogue Page</h1>;
+import {useState} from "react";
+
+let AllCats = await GetCats();
+
+function Search(){
+    const [text, setText] = useState("");
+
+    const handleSearch = (e) => {
+        setText(e.target.value);
+    };
+
+    const filteredSearch = AllCats.filter((cat) => 
+        cat.breeds[0].name.toLowerCase().includes(text.toLowerCase())
+    );
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={text}
+                onChange={handleSearch}
+                placeholder="Search..."
+            />
+            <p>Searching For: {text === "" ? "All" : text}</p>
+        </div>
+    )
 }
