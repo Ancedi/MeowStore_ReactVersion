3a7e70713b38353bc42d38322df8ddd6ec227c05
Ancedi <patrickhombrebueno@gmail.com>
2026-05-21 11:43:23 +0200

    temp commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter, Routes, Route, Link, Outlet, NavLink, useParams} from 're
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Catalogue from './pages/Catalogue';
 
 // Global nav link styles — applies active/inactive states to navigation links
 import './css/NavLinkStyles.css';
@@ -14,11 +15,13 @@ function App() {
     <BrowserRouter>
       <nav className="nav">
         <NavLink to="/" className="nav-link">Home</NavLink> |{" "}
+        <NavLink to="/catalogue" className="nav-link">Catalogue</NavLink> |{" "}
         <NavLink to="/about" className="nav-link">About</NavLink> |{" "}
         <NavLink to="/contact" className="nav-link">Contact</NavLink> |{" "}
       </nav>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/catalogue" element={<Catalogue/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
       </Routes>
